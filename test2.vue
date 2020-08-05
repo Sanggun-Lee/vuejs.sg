4defc96fd90692d3d91d82fc9465a12517a848d0
sanggun <sglee@qesg.co.kr>
2020-08-05 13:42:47 +0900

    sg2

---
+++ test2.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+<script>
+export default {
+    data(){
+        return {}
+    },
+    methods {
+        addValue: function () {
+            this
+        },
+        addValue2() {
+            this
+        }
+    }
+}
+</script>
